c88bad77ae7c717629dea74459ed0b3379c19745
ByCGS1999 <66258488+ByCGS1999@users.noreply.github.com>
2023-12-02 23:46:46 +0100

    Fixed everything, again

--- GoldEngine-Internal/Window.cpp
+++ GoldEngine-Internal/Window.cpp
@@ -1,5 +1,16 @@
 #include "Window.h"
 
+#include "Includes.h"
+#include "ScriptSandbox.h"
+#include "CastToNative.h"
+#include "DataPacks.h"
+#include "Drawing.h"
+
+
+int tickRate = 0;
+int physicsTick = 60;
+int targetFPS = 60;
+
 void Engine::Window::SetWindowFlags(unsigned int flags)
 {
 	SetConfigFlags(flags);
@@ -9,6 +20,13 @@ void Engine::Window::OpenWindow(int width, int height, System::String^ name)
 {
 	DataPacks::BeginDataPacks();
 	InitWindow(width, height, CastToNative(name));
+	rlImGuiSetup(true);
+}
+
+void Engine::Window::OpenWindow(int width, int height, const char* name)
+{
+	DataPacks::BeginDataPacks();
+	InitWindow(width, height, name);
 	rlImGuiSetup(true);
 }
 
@@ -35,6 +53,35 @@ int Engine::Window::GetFPS()
 void Engine::Window::Exit()
 {
 
+}
+
+void Engine::Window::Draw()
+{
+	SetTargetFPS(targetFPS);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		for each (System::Object ^ object in drawList)
+		{
+			System::Action^ invokable = (System::Action^)object;
+			invokable->Invoke();
+		}
+
+		EndDrawing();
+
+		rlImGuiBegin();
+		DrawFPS(0, 0);
+		ImGui::ShowDemoWindow((bool*)true);
+		rlImGuiEnd();
+
+		tickRate++;
+	}
+
+	rlImGuiShutdown();
+	Exit();
+	CloseWindow();
 }
 
 void Engine::Window::Draw(System::Action<int, int>^ executionStack)
