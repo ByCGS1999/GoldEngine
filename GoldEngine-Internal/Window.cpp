173d6c983c27331f3ddbf20a6f126eab819d5eb2
ByCGS1999 <66258488+ByCGS1999@users.noreply.github.com>
2023-12-02 23:17:00 +0100

    Updated Window management file, added header and restructurated source code. Ready for c++ abstraction

--- GoldEngine-Internal/Window.cpp
+++ GoldEngine-Internal/Window.cpp
@@ -1,100 +1,70 @@
-#define RAYGUI_IMPLEMENTATION
-#define _CRT_SECURE_NO_WARNINGS
-#define SUPPORT_FILEFORMAT_HDR
+#include "Window.h"
 
-#include "Includes.h"
-#include "ScriptSandbox.h"
-#include "CastToNative.h"
-#include "DataPacks.h"
-#include "Drawing.h"
-
-using namespace System;
-
-namespace Engine
+void Engine::Window::SetWindowFlags(unsigned int flags)
 {
-	public ref class Window
+	SetConfigFlags(flags);
+}
+
+void Engine::Window::OpenWindow(int width, int height, System::String^ name)
+{
+	DataPacks::BeginDataPacks();
+	InitWindow(width, height, CastToNative(name));
+	rlImGuiSetup(true);
+}
+
+void Engine::Window::AddToDraw(System::Action^ invokable)
+{
+	drawList.Add(invokable);
+}
+
+void Engine::Window::ResetPhysics()
+{
+	tickRate = 0;
+}
+
+void Engine::Window::SetFPS(int fps)
+{
+	targetFPS = fps;
+}
+
+int Engine::Window::GetFPS()
+{
+	return targetFPS;
+}
+
+void Engine::Window::Exit()
+{
+
+}
+
+void Engine::Window::Draw(System::Action<int, int>^ executionStack)
+{
+	SetTargetFPS(targetFPS);
+
+	while (!WindowShouldClose())
 	{
-	private: 
-		static int tickRate = 0;
-		static int physicsTick = 60;
-		static int targetFPS = 60;
-		static ArrayList drawList;
+		// -- update
+		executionStack->Invoke(tickRate, physicsTick);
 
-	public:
-		void SetWindowFlags(unsigned int flags)
+		BeginDrawing();
+
+		for each (System::Object ^ object in drawList)
 		{
-			SetConfigFlags(flags);
+			System::Action^ invokable = (System::Action^)object;
+			invokable->Invoke();
 		}
 
-	public:
-		void OpenWindow(int width, int height, System::String^ name)
-		{
-			DataPacks::BeginDataPacks();
-			InitWindow(width, height, CastToNative(name));
-			rlImGuiSetup(true);
-		}
+		EndDrawing();
 
-	public:
-		void AddToDraw(System::Action^ invokable)
-		{
-			drawList.Add(invokable);
-		}
+		rlImGuiBegin();
+		DrawFPS(0, 0);
+		ImGui::ShowDemoWindow((bool*)true);
+		rlImGuiEnd();
 
-	public:
-		void ResetPhysics()
-		{
-			tickRate = 0;
-		}
+		tickRate++;
+	}
 
-	public:
-		void SetFPS(int fps)
-		{
-			targetFPS = fps;
-		}
-
-	public:
-		int GetFPS()
-		{
-			return targetFPS;
-		}
-
-	public:
-		virtual void Exit()
-		{
-
-		}
-
-	public:
-		void Draw(System::Action<int,int>^ executionStack)
-		{
-			SetTargetFPS(targetFPS);
-
-			while (!WindowShouldClose())
-			{
-				// -- update
-				executionStack->Invoke(tickRate, physicsTick);
-
-				BeginDrawing();
-
-				for each (System::Object ^ object in drawList)
-				{
-					System::Action^ invokable = (System::Action^)object;
-					invokable->Invoke();
-				}
-
-				EndDrawing();
-
-				rlImGuiBegin();
-				DrawFPS(0, 0);
-				ImGui::ShowDemoWindow((bool*)true);
-				rlImGuiEnd();
-
-				tickRate++;
-			}
-
-			rlImGuiShutdown();
-			Exit();
-			CloseWindow();
-		}
-	};
+	rlImGuiShutdown();
+	Exit();
+	CloseWindow();
 }
