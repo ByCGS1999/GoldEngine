de12f4d1297b316501b333af1f61ebb56865fb02
ByCGS1999 <66258488+ByCGS1999@users.noreply.github.com>
2024-03-10 19:05:42 +0100

    update comments

--- GoldEngine-Internal/ShaderManager.h
+++ GoldEngine-Internal/ShaderManager.h
@@ -22,13 +22,13 @@ public:
 		Engine::Assets::Storage::DataPacks::singleton().AddShader(shaderId, ::LoadShaderFromMemory(CastStringToNative(vertexShader).c_str(), CastStringToNative(fragmentShader).c_str()));
 	}
 
-	// load a shader into datapacks
+	// update a shader into datapacks \\
+
 	void UpdateShaderFromFile(unsigned int shaderId, String^ vsFp, String^ fsFp)
 	{
 		Engine::Assets::Storage::DataPacks::singleton().SetShader(shaderId, LoadShader(CastStringToNative(vsFp).c_str(), CastStringToNative(fsFp).c_str()));
 	}
 
-	// mainly used for pushing modified shader code into the datapacks
 	void UpdateShaderFromMemory(unsigned int shaderId, String^ vertexShader, String^ fragmentShader)
 	{
 		Engine::Assets::Storage::DataPacks::singleton().SetShader(shaderId, ::LoadShaderFromMemory(CastStringToNative(vertexShader).c_str(), CastStringToNative(fragmentShader).c_str()));
