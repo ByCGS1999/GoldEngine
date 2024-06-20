74482e9f101e7adf6e51cf363c2085e93614aa12
CGS <66258488+ByCGS1999@users.noreply.github.com>
2024-06-21 01:38:25 +0200

    Delete build from src

--- Build/x64/PlaybackEngine_Release/Data/Lua/src/test.lua
+++
@@ -1,9 +0,0 @@
-function Start()
-	attributes.removeAttribute("Test Attribute");
-	attributes.setAttribute(Attribute.create("Test Attribute", "Hello world"));
-
-	Logging.Log(attributes.getAttribute("Test Attribute").getValue());
-end
-
-
-
